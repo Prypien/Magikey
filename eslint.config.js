4b43af8c169cf57f71256bc762e15b7c2f645a1f
Prypien <jen@preisser.de>
2025-06-25 14:48:03 +0200

    Innitial Commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,31 @@
-// Moderne ESLint-Konfiguration
+// eslint.config.js
 import eslint from '@eslint/js'
 import vue from 'eslint-plugin-vue'
+import parser from 'vue-eslint-parser'
 
 export default [
   {
-    files: ['**/*.js', '**/*.vue'],
-    ignores: ['node_modules'],
-    languageOptions: { ecmaVersion: 2021, sourceType: 'module' },
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser, // Vue-Parser aktivieren
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
     plugins: { vue },
     rules: {
       ...eslint.configs.recommended.rules,
-      ...vue.configs['vue3-essential'].rules
-    }
-  }
+      ...vue.configs['vue3-essential'].rules,
+    },
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
+    plugins: { vue },
+    rules: {
+      ...eslint.configs.recommended.rules,
+    },
+  },
 ]
-
-// Weitere Konfigurationen können hier angefügt werden
